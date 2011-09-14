ef8090f531fe1b4f94459792eed474b02b72f13a
Tim Ross <tim@timross.info>
2011-09-14 16:17:58 +1200

    Added params to find

--- lib/ruxero/base_model/finders.rb
+++ lib/ruxero/base_model/finders.rb
@@ -1,7 +1,11 @@
 class Ruxero::BaseModel
 
-  def self.all(*args)
-    result = Ruxero.application.get("/api.xro/2.0/#{pluralized_name}")
+  def self.all(params = {})
+    path = "/api.xro/2.0/#{pluralized_name}"
+    if params.any?
+      path += "?" + params.map {|key,value| "#{CGI.escape(key.to_s)}=#{CGI.escape(value.to_s)}"}.join("&")
+    end
+    result = Ruxero.application.get(path)
     parse_collection(result) || Array.new
   end
 
